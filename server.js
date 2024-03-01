2dd35869cf4fdf58cdc636f811293c785f93e3b2
bonkizu <153252467+bonkizu@users.noreply.github.com>
2024-03-01 16:26:14 +0700

    Revert "Update server.js"

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-dotenv.config({ path: './config/config.env' });
-const PORT = process.env.PORT || 5000;
+dotenv.config({path:'./config/config.env'});
+const PORT = process.env.PORT||5000;
 const connectDB = require('./config/db.js');
+
 
 connectDB();
 
-// test 1234
 
-const server = app.listen(
-  PORT,
-  console.log('Sever runnig in', process.env.NODE_ENV, ' mode on port ', PORT)
-);
 
-app.get('/', (req, res) => {
-  res.status(200).json({ success: true, data: { id: 1 } });
+
+const server = app.listen(PORT, console.log('Sever runnig in',process.env.NODE_ENV,' mode on port ', PORT));
+
+
+
+
+
+app.get('/',(req,res) => {
+    res.status(200).json({success:true,data:{id:1}});
 });
