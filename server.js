c0c91a04e150ffed94d8a2a0f6121dfe69d23ae8
bonkizu <isukaomu@gmail.com>
2024-03-01 16:55:17 +0700

    test put

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-dotenv.config({path:'./config/config.env'});
-const PORT = process.env.PORT||5000;
+dotenv.config({ path: './config/config.env' });
+const PORT = process.env.PORT || 5000;
 const connectDB = require('./config/db.js');
-
 
 connectDB();
 
+const server = app.listen(
+  PORT,
+  console.log('Sever runnig in', process.env.NODE_ENV, ' mode on port ', PORT)
+);
 
-
-
-const server = app.listen(PORT, console.log('Sever runnig in',process.env.NODE_ENV,' mode on port ', PORT));
-
-
-
-
-
-app.get('/',(req,res) => {
-    res.status(200).json({success:true,data:{id:1}});
+app.get('/', (req, res) => {
+  res.status(200).json({ success: true, data: { id: 1 } });
 });
 
-app.delete('/',(req,res) => {
-    res.status(200).json({success:true,data:{id:1}});
+app.delete('/', (req, res) => {
+  res.status(200).json({ success: true, data: { id: 1 } });
+});
+
+app.put('/', (req, res) => {
+  res.status(200).json({ success: true, data: { id: 1 } });
 });
