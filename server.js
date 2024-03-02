b5a19fcf87e06751591172f9c0bd137689948167
bonkizu <isukaomu@gmail.com>
2024-03-02 11:43:05 +0700

    add expressJson and cookieParser

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const providers = require('./routes/providers.js');
 const auth = require('./routes/auth');
 const bookings = require('./routes/bookings.js');
+const cookieParser = require('cookie-parser');
 
 dotenv.config({ path: './config/config.env' });
 const PORT = process.env.PORT || 5000;
@@ -11,21 +12,21 @@ const connectDB = require('./config/db.js');
 
 connectDB();
 
+app.use(express.json());
+
+app.use(cookieParser());
+
 const server = app.listen(
   PORT,
   console.log('Sever runnig in', process.env.NODE_ENV, ' mode on port ', PORT)
 );
 
 //Handle unhandled promise rejection
-process.on('unhandledRejection',(err,promise)=>{
+process.on('unhandledRejection', (err, promise) => {
   console.log(`Error: ${err.message}`);
-  server.close(()=>process.exit(1));
+  server.close(() => process.exit(1));
 });
 
-
-app.use('/api/v1/providers',providers);
-app.use('/api/v1/auth',auth);
-app.use('/api/v1/bookings',bookings);
-
-
-
+app.use('/api/v1/providers', providers);
+app.use('/api/v1/auth', auth);
+app.use('/api/v1/bookings', bookings);
